a7b01e3bdadd52239fe29ed64d00e5b54469a150
AlexandreKnaff <alexknaff@gmail.com>
2014-12-28 22:17:31 +0100

    First Commit from Alex
    link main activity to question activity

--- app/src/main/java/jeremie/rallyproject/MainActivity.java
+++ app/src/main/java/jeremie/rallyproject/MainActivity.java
@@ -1,9 +1,14 @@
 package jeremie.rallyproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,7 +18,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +39,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void startRally(View view){
+        Intent intent = new Intent(this, QuestionActivity.class);
+        startActivity(intent);
     }
 }
