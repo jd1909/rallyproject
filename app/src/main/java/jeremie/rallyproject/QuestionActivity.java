73aa15fd6aa6366a5a1595a164739e6d68247c85
jd1909 <jerm.dauphin@gmail.com>
2014-12-29 21:36:06 +0100

    Popup window update
    Update to the popup window, it now works correctly

--- app/src/main/java/jeremie/rallyproject/QuestionActivity.java
+++ app/src/main/java/jeremie/rallyproject/QuestionActivity.java
@@ -1,10 +1,16 @@
 package jeremie.rallyproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
@@ -32,6 +38,33 @@ public class QuestionActivity extends ActionBarActivity {
 
         // Zoom in, animating the camera.
         map.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+
+        final Button btnOpenPopup = (Button)findViewById(R.id.button2);
+        btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater
+                        = (LayoutInflater)getBaseContext()
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.activity_popup, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        popupWindow.dismiss();
+                    }});
+
+                popupWindow.showAsDropDown(findViewById(R.id.textView2), 50, -30);
+
+            }});
     }
 
 
@@ -57,9 +90,5 @@ public class QuestionActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void confirm(View view){
-
-
-    }
 
 }
