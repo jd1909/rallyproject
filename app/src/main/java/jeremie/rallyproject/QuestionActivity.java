0de093c5aee2026904756114ab1ec7f52eaab516
jd1909 <jerm.dauphin@gmail.com>
2014-12-31 17:55:07 +0100

    updated QuestionActivity

--- app/src/main/java/jeremie/rallyproject/QuestionActivity.java
+++ app/src/main/java/jeremie/rallyproject/QuestionActivity.java
@@ -3,6 +3,7 @@ package jeremie.rallyproject;
 import android.content.Context;
 import android.content.Intent;
 import android.database.SQLException;
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,19 +28,24 @@ import java.util.Random;
 
 public class QuestionActivity extends ActionBarActivity {
     private GoogleMap map;
-    static final LatLng LUXEMBOURG = new LatLng(49.6117,6.1300);
-    static final LatLng CLAIREFONTAINE = new LatLng(49.6098,6.1325);
+    static final LatLng LUXEMBOURG = new LatLng(49.6117, 6.1300);
+    static final LatLng CLAIREFONTAINE = new LatLng(49.6098, 6.1325);
     private SimpleQuestions SQ1 = new SimpleQuestions(1, " How much does the five course menu cost in the famous restaurant Clairefontaine?",
-            "95 euros", (float)49.60985, (float)6.132561);
+            "95 euros", (float) 49.60985, (float) 6.132561);
     private SimpleQuestions SQ2 = new SimpleQuestions(2, "Its Saturday morning 7 am, you are hungry, where can you go for a burger?",
-            "Saumur Crystal Club", (float)49.604201, (float)6.129737);
+            "Saumur Crystal Club", (float) 49.604201, (float) 6.129737);
     private TextView txtQuestion;
     private EditText edtAnswer;
+    private Button verify;
+    private Button answer;
     private ScoreCounter count = new ScoreCounter();
     private List<SimpleQuestions> questions = new ArrayList<>();
+    private Marker myLocation;
+    private boolean verified;
 
- //   Random rand = new Random();
- //   int Random = rand.nextInt(1) +1;
+
+    //   Random rand = new Random();
+    //   int Random = rand.nextInt(1) +1;
     private Random rn = new Random();
     public int random;
     public SimpleQuestions SQR;
@@ -51,6 +57,7 @@ public class QuestionActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         //add simplequestion objects to list.
+
         questions.add(SQ1);
         questions.add(SQ2);
         random = rn.nextInt(questions.size());
@@ -58,14 +65,17 @@ public class QuestionActivity extends ActionBarActivity {
 
 
 
+
         float lat = SQR.getLatitude();
         float lng = SQR.getLongitude();
+        answer = (Button)findViewById(R.id.button2);
 
         txtQuestion = (TextView) findViewById(R.id.textView3);
         edtAnswer = (EditText) findViewById(R.id.editText);
-        txtQuestion.setText(SQR.getQuestion());
+        txtQuestion.setText("");
+        answer.setEnabled(false);
 
-        LatLng location = new LatLng(lat,lng);
+        LatLng location = new LatLng(lat, lng);
 
 
         // Set up the google map fragment
@@ -82,45 +92,48 @@ public class QuestionActivity extends ActionBarActivity {
 
         // Zoom in, animating the camera.
         map.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+        this.map.setMyLocationEnabled(true);
+        this.myLocation= map.addMarker(new MarkerOptions().position(new LatLng(49.6117, 6.1300)).title("You are here"));
+        this.map.setOnMyLocationChangeListener(this.myLocationChangeListener);
+
+
 /**
-        final Button btnOpenPopup = (Button)findViewById(R.id.button2);
-        btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
+ final Button btnOpenPopup = (Button)findViewById(R.id.button2);
+ btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
 
-            @Override
-            public void onClick(View arg0) {
-                LayoutInflater layoutInflater
-                        = (LayoutInflater)getBaseContext()
-                        .getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupView = layoutInflater.inflate(R.layout.activity_popup, null);
-                final PopupWindow popupWindow = new PopupWindow(
-                        popupView,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT);
+@Override public void onClick(View arg0) {
+LayoutInflater layoutInflater
+= (LayoutInflater)getBaseContext()
+.getSystemService(LAYOUT_INFLATER_SERVICE);
+View popupView = layoutInflater.inflate(R.layout.activity_popup, null);
+final PopupWindow popupWindow = new PopupWindow(
+popupView,
+ViewGroup.LayoutParams.WRAP_CONTENT,
+ViewGroup.LayoutParams.WRAP_CONTENT);
 
-                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
-                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+btnDismiss.setOnClickListener(new Button.OnClickListener(){
 
-                    @Override
-                    public void onClick(View v) {
-                        // TODO Auto-generated method stub
-                        popupWindow.dismiss();
+@Override public void onClick(View v) {
+// TODO Auto-generated method stub
+popupWindow.dismiss();
 
-                    }});
+}});
 
-                popupWindow.showAsDropDown(findViewById(R.id.textView2), 50, -30);
+popupWindow.showAsDropDown(findViewById(R.id.textView2), 50, -30);
 
-            }});
+}});
  */
     }
-    public void Answer(View view){
-        if(SQR.getAnswer().equals(edtAnswer.getText().toString()))
-        {
-                count.increment();
+
+    public void Answer(View view) {
+        if (SQR.getAnswer().equals(edtAnswer.getText().toString())) {
+            count.increment();
 
         }
         Intent intent = new Intent(this, MultiQuestionActivity.class);
         int score = count.totalScore();
-        intent.putExtra("Score",score);
+        intent.putExtra("Score", score);
         startActivity(intent);
     }
 
@@ -146,6 +159,39 @@ public class QuestionActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public boolean verifyLocation() {
+        LatLng quest = new LatLng(SQR.getLatitude(), SQR.getLongitude());
+        if (myLocation.getPosition().latitude == SQR.getLatitude()&& myLocation.getPosition().longitude == SQR.getLongitude() )
+            return true;
+
+        return false;
+    }
+
+
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng newLoc = new LatLng(location.getLatitude(), location.getLongitude());
+            myLocation.setPosition(newLoc);
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(newLoc, 16.0f));
+            if (verifyLocation()){
+                questions.add(SQ1);
+                questions.add(SQ2);
+                random = rn.nextInt(questions.size());
+                SQR = questions.get(random);
+                verify.setEnabled(false);
+                answer.setEnabled(true);
+                txtQuestion.setText(SQR.getQuestion());
+
+
+            }
+        }
+    };
+
 
 
 }
